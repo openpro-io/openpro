10035fb62230a477bb431f6f6cf68d17c9b5ecf1
claygorman <claygorman@gmail.com>
2023-12-23 12:39:17 -0800

    Update UI color scheme in IssueModal
    Altered several CSS class definitions in the LinkIssueSearch component of the IssueModal. These changes focus on updating the UI color scheme, transitioning from a white/grey and teal scheme to a "surface-overlay" and "primary" color scheme. The outcome maintains the accessibility in the color contrasts, while creating a more unified visual user experience.

--- frontend/components/IssueModal/IssueLinkedIssues/LinkIssueSearch.tsx
+++ frontend/components/IssueModal/IssueLinkedIssues/LinkIssueSearch.tsx
@@ -57,9 +57,9 @@ const LinkIssueSearch = ({
     <div className='w-full min-w-full'>
       <Combobox value={selected} onChange={setSelected}>
         <div className='relative mt-1 rounded-lg border border-primary/20'>
-          <div className='relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
+          <div className='relative w-full cursor-default overflow-hidden rounded-lg bg-surface-overlay text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
             <Combobox.Input
-              className='w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
+              className='w-full border-none bg-surface-overlay py-2 pl-3 pr-10 text-sm leading-5 text-primary focus:ring-0'
               displayValue={(issue: any) => issue?.title}
               onChange={debouncedResults}
               placeholder='Search for an issue'
@@ -78,9 +78,9 @@ const LinkIssueSearch = ({
             leaveTo='opacity-0'
             afterLeave={() => setQuery('')}
           >
-            <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm'>
+            <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-surface-overlay py-1 shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm'>
               {filteredIssues.length === 0 && query !== '' ? (
-                <div className='relative cursor-default select-none px-4 py-2 text-gray-700'>
+                <div className='relative cursor-default select-none px-4 py-2 text-primary'>
                   Nothing found.
                 </div>
               ) : (
@@ -88,8 +88,10 @@ const LinkIssueSearch = ({
                   <Combobox.Option
                     key={issue.id}
                     className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-2 pr-4 ${
-                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      `relative cursor-default select-none py-2 pl-8 pr-4 ${
+                        active
+                          ? 'bg-surface-overlay-hovered text-primary'
+                          : 'text-primary'
                       }`
                     }
                     value={issue}
@@ -105,8 +107,8 @@ const LinkIssueSearch = ({
                         </span>
                         {selected ? (
                           <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-teal-600'
+                            className={`absolute inset-y-0 left-0 flex items-center pl-1 ${
+                              active ? 'text-primary' : 'text-blue-500'
                             }`}
                           >
                             <CheckIcon className='h-5 w-5' aria-hidden='true' />
