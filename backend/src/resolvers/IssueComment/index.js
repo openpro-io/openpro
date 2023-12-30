4f870d182136d359b1fc133c403b5391cb84bffa
RokasVaitkevicius <vaitkevicius.rokas@gmail.com>
2023-12-30 14:53:07 +0200

    Merge mistakes fixed

--- backend/src/resolvers/IssueComment/index.js
+++ backend/src/resolvers/IssueComment/index.js
@@ -1,6 +1,47 @@
 const resolvers = {
   Query: {},
-  Mutation: {},
+  Mutation: {
+    createIssueComment: async (parent, { input }, { db, user }) => {
+      const { issueId, comment } = input;
+      const reporterId = user.id;
+
+      return await db.sequelize.models.IssueComment.create({
+        issueId,
+        reporterId,
+        comment,
+      });
+    },
+    deleteIssueComment: async (parent, { input }, { db }) => {
+      const { commentId } = input;
+
+      const findIssueComment = await db.sequelize.models.IssueComment.findByPk(commentId);
+
+      if (!findIssueComment) {
+        throw new Error('Issue comment not found');
+      }
+
+      await findIssueComment.destroy();
+
+      return {
+        message: 'deleted comment',
+        status: 'success',
+      };
+    },
+    updateIssueComment: async (parent, { input }, { db }) => {
+      const { commentId, comment } = input;
+
+      const findIssueComment = await db.sequelize.models.IssueComment.findByPk(commentId);
+
+      if (!findIssueComment) {
+        throw new Error('Issue comment not found');
+      }
+
+      findIssueComment.comment = comment;
+      await findIssueComment.save();
+
+      return findIssueComment;
+    },
+  },
   IssueComment: {
     reporter: (parent, args, { db }) => {
       return db.sequelize.models.User.findByPk(parent.reporterId);
