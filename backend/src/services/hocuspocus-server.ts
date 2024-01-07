3b7d5f7fff9e66bcb844a162027cf1ddd5b4516a
claygorman <claygorman@gmail.com>
2024-01-06 20:36:13 -0800

    Refactor hocuspocus-server.ts and add caching
    The code refactor primarily targets improving readability by breaking the server configuration into discrete functions. Additionally, a cache mechanism has been introduced for token verification process. This adjustment aims to boost performance by reducing redundant requests to JWT verification API whenever a valid token is presented.

--- backend/src/services/hocuspocus-server.ts
+++ backend/src/services/hocuspocus-server.ts
@@ -1,135 +1,146 @@
-import { Server } from '@hocuspocus/server';
-import { FRONTEND_HOSTNAME, HTTP_PORT } from './config.js';
 import { Database } from '@hocuspocus/extension-database';
-import db from '../db/index.js';
+import { Server } from '@hocuspocus/server';
 import axios from 'axios';
+
+import db from '../db/index.js';
+import { cache } from './cache';
+import { FRONTEND_HOSTNAME, HTTP_PORT } from './config.js';
+import { hash } from './utils';
+
+const fetch = async ({ documentName }): Promise<Uint8Array | null> => {
+  return new Promise(async (resolve, reject) => {
+    const [entityType, entityId, entityField] = documentName.split('.');
+
+    if (entityType === 'issue' && entityField === 'description') {
+      let issue = null;
+
+      try {
+        issue = await db.sequelize.models.Issue.findOne({
+          where: {
+            id: entityId,
+          },
+        });
+      } catch (e) {
+        reject(e);
+      }
+
+      if (!issue || !issue?.descriptionRaw) resolve(null);
+
+      const descriptionAsUnit8Array = new Uint8Array(issue.descriptionRaw);
+      resolve(descriptionAsUnit8Array.length === 0 ? null : descriptionAsUnit8Array);
+    }
+
+    if (entityType === 'issueComment' && entityField === 'comment') {
+      // @ts-ignore
+      if (!isFinite(entityId)) return resolve(null);
+
+      const comment = await db.sequelize.models.IssueComment.findOne({
+        where: {
+          id: entityId,
+        },
+      });
+
+      // @ts-ignore
+      if (!comment || !comment?.commentRaw) return resolve(null);
+
+      // @ts-ignore
+      const commentAsUnit8Array = new Uint8Array(comment.commentRaw);
+      resolve(commentAsUnit8Array.length === 0 ? null : commentAsUnit8Array);
+    }
+
+    resolve(null);
+  });
+};
+
+const store = async ({ documentName, state }) => {
+  const [entityType, entityId, entityField] = documentName.split('.');
+
+  if (entityType === 'issue' && entityField === 'description') {
+    await db.sequelize.models.Issue.update(
+      {
+        descriptionRaw: state,
+      },
+      {
+        where: {
+          id: entityId,
+        },
+      }
+    );
+  }
+
+  if (entityType === 'issueComment' && entityField === 'comment') {
+    // @ts-ignore
+    if (!isFinite(entityId)) return;
+
+    await db.sequelize.models.IssueComment.update(
+      {
+        commentRaw: state,
+      },
+      {
+        where: {
+          id: entityId,
+        },
+      }
+    );
+  }
+};
+
+const onAuthenticate = async (data: any) => {
+  let token = data?.token;
+
+  if (!token) {
+    console.warn('Throwing exception to tiptap user');
+    throw new Error('Not authorized!');
+  }
+
+  token = `Bearer ${token}`;
+
+  const cacheKey = hash(token);
+
+  const cachedUser = cache.get(cacheKey);
+
+  if (cachedUser) return { user: db.User.build(cachedUser).toJSON() };
+
+  let verifyJwt = {};
+
+  try {
+    verifyJwt = await axios.get(`${FRONTEND_HOSTNAME}/api/verify-jwt`, {
+      headers: {
+        authorization: token,
+      },
+    });
+  } catch (e) {
+    throw new Error('Unable to verify tiptap user!');
+  }
+
+  const {
+    // @ts-ignore
+    data: { provider, sub },
+  } = verifyJwt;
+
+  const externalId = `${provider}__${sub}`;
+
+  const user = await db.sequelize.models.User.findOne({
+    where: { externalId },
+  });
+
+  cache.set(cacheKey, user, 5);
+
+  return {
+    user,
+  };
+};
 
 const hocuspocusServer = Server.configure({
   port: Number(HTTP_PORT),
   address: '0.0.0.0',
   extensions: [
     new Database({
-      fetch: ({ documentName }) => {
-        return new Promise(async (resolve, reject) => {
-          const [entityType, entityId, entityField] = documentName.split('.');
-
-          if (entityType === 'issue' && entityField === 'description') {
-            let issue = null;
-
-            try {
-              issue = await db.sequelize.models.Issue.findOne({
-                where: {
-                  id: entityId,
-                },
-              });
-            } catch (e) {
-              reject(e);
-            }
-
-            if (!issue || !issue?.descriptionRaw) resolve(null);
-
-            const descriptionAsUnit8Array = new Uint8Array(
-              issue.descriptionRaw
-            );
-            resolve(
-              descriptionAsUnit8Array.length === 0
-                ? null
-                : descriptionAsUnit8Array
-            );
-          }
-
-          if (entityType === 'issueComment' && entityField === 'comment') {
-            // @ts-ignore
-            if (!isFinite(entityId)) return resolve(null);
-
-            const comment = await db.sequelize.models.IssueComment.findOne({
-              where: {
-                id: entityId,
-              },
-            });
-
-            // @ts-ignore
-            if (!comment || !comment?.commentRaw) return resolve(null);
-
-            // @ts-ignore
-            const commentAsUnit8Array = new Uint8Array(comment.commentRaw);
-            resolve(
-              commentAsUnit8Array.length === 0 ? null : commentAsUnit8Array
-            );
-          }
-
-          resolve(null);
-        });
-      },
-      store: async ({ documentName, state }) => {
-        const [entityType, entityId, entityField] = documentName.split('.');
-
-        if (entityType === 'issue' && entityField === 'description') {
-          await db.sequelize.models.Issue.update(
-            {
-              descriptionRaw: state,
-            },
-            {
-              where: {
-                id: entityId,
-              },
-            }
-          );
-        }
-
-        if (entityType === 'issueComment' && entityField === 'comment') {
-          // @ts-ignore
-          if (!isFinite(entityId)) return;
-
-          await db.sequelize.models.IssueComment.update(
-            {
-              commentRaw: state,
-            },
-            {
-              where: {
-                id: entityId,
-              },
-            }
-          );
-        }
-      },
+      fetch,
+      store,
     }),
   ],
-  async onAuthenticate(data) {
-    const { token } = data;
-
-    if (!token) {
-      console.warn('Throwing exception to tiptap user');
-      throw new Error('Not authorized!');
-    }
-
-    let verifyJwt = {};
-
-    try {
-      verifyJwt = await axios.get(`${FRONTEND_HOSTNAME}/api/verify-jwt`, {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      });
-    } catch (e) {
-      throw new Error('Unable to verify tiptap user!');
-    }
-
-    const {
-      // @ts-ignore
-      data: { provider, sub },
-    } = verifyJwt;
-
-    const externalId = `${provider}__${sub}`;
-
-    const user = await db.sequelize.models.User.findOne({
-      where: { externalId },
-    });
-
-    return {
-      user: user ? user.toJSON() : null,
-    };
-  },
+  onAuthenticate,
 });
 
 export default hocuspocusServer;
