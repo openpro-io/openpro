ee766c3454103ea692c7dcffd6440118361fefcf
claygorman <claygorman@gmail.com>
2024-01-11 16:16:14 -0800

    Refactor websocket handler in Fastify server
    The handshake connection and message handling is now encapsulated within a separate `wsPlugin` function. This streamlines the main `fastifyWsHandler` function by allowing it to focus on fastify-specific configuration and registration tasks, improving code readability and maintainability.

--- backend/src/server/fastify-ws.ts
+++ backend/src/server/fastify-ws.ts
@@ -12,53 +12,55 @@ interface MyWebSocket extends WebSocket {
   namespace?: string;
 }
 
+const wsPlugin = async (instance: FastifyInstance) => {
+  instance.websocketServer.on('connection', function connection(ws: MyWebSocket) {
+    ws.isAlive = true;
+
+    // Heartbeat
+    ws.on('message', function (message) {
+      const msg = message.toString();
+      if (['ping', 'pong'].includes(msg)) {
+        ws.isAlive = true;
+        ws.send(msg === 'ping' ? 'pong' : 'ping');
+      }
+    });
+  });
+
+  const interval = setInterval(function ping() {
+    instance.websocketServer.clients.forEach(function each(ws: MyWebSocket) {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+    });
+  }, 30000);
+
+  instance.websocketServer.on('close', function close() {
+    clearInterval(interval);
+  });
+
+  instance.get('/ws', { websocket: true }, (connection, req) => {
+    const context = {};
+
+    const clients = instance.websocketServer.clients;
+
+    const handleConnectionSocket: MyWebSocket = connection.socket;
+    handleConnectionSocket.id = nanoid();
+    handleConnectionSocket.user = req?.user;
+    handleConnectionSocket.namespace = 'ws';
+
+    wsServer.handleConnection({
+      socket: handleConnectionSocket,
+      req,
+      context,
+      clients,
+    });
+  });
+};
+
 export const fastifyWsHandler = async (fastify: FastifyInstance) => {
   await fastify.register(fastifyWebsocket, {
     options: { maxPayload: 1048576, clientTracking: true },
   });
 
-  await fastify.register(async function (fastify) {
-    fastify.websocketServer.on('connection', function connection(ws: MyWebSocket) {
-      ws.isAlive = true;
-
-      // Heartbeat
-      ws.on('message', function (message) {
-        const msg = message.toString();
-        if (['ping', 'pong'].includes(msg)) {
-          ws.isAlive = true;
-          ws.send(msg === 'ping' ? 'pong' : 'ping');
-        }
-      });
-    });
-
-    const interval = setInterval(function ping() {
-      fastify.websocketServer.clients.forEach(function each(ws: MyWebSocket) {
-        if (ws.isAlive === false) return ws.terminate();
-
-        ws.isAlive = false;
-      });
-    }, 30000);
-
-    fastify.websocketServer.on('close', function close() {
-      clearInterval(interval);
-    });
-
-    fastify.get('/ws', { websocket: true }, (connection, req) => {
-      const context = {};
-
-      const clients = fastify.websocketServer.clients;
-
-      const handleConnectionSocket: MyWebSocket = connection.socket;
-      handleConnectionSocket.id = nanoid();
-      handleConnectionSocket.user = req?.user;
-      handleConnectionSocket.namespace = 'ws';
-
-      wsServer.handleConnection({
-        socket: handleConnectionSocket,
-        req,
-        context,
-        clients,
-      });
-    });
-  });
+  await fastify.register(wsPlugin);
 };
